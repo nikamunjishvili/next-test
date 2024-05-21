2e89fff357264f63551eb976e90e18b0f5a01e52
Nika munjisvili <nikamunja31415@gmail.com>
2024-05-21 19:42:59 +0400

    fix code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis
         ullam id voluptates ducimus quis iste porro quod sunt saepe
